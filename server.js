693c55bf62522410acf9ec2edcc75ea166e502db
dhalladay <dave.halladay@outlook.com>
2022-01-11 18:27:48 -0700

    update code

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mysql = require('mysql2');
-const inputCheck = require('./utils/inputCheck');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -13,7 +13,9 @@ app.use(express.json());
 const db = mysql.createConnection(
   {
     host: 'localhost',
+    // Your MySQL username,
     user: 'root',
+    // Your MySQL password
     password: '',
     database: 'election'
   },
@@ -53,7 +55,7 @@ app.get('/api/candidate/:id', (req, res) => {
   });
 });
 
-//delete candidate
+// Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
   const sql = `DELETE FROM candidates WHERE id = ?`;
   const params = [req.params.id];
@@ -75,15 +77,21 @@ app.delete('/api/candidate/:id', (req, res) => {
   });
 });
 
-//create a candidate
+// Create a candidate
 app.post('/api/candidate', ({ body }, res) => {
-  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
   if (errors) {
     res.status(400).json({ error: errors });
-    return
+    return;
   }
+
   const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-  values (?,?,?)`;
+    VALUES (?,?,?)`;
   const params = [body.first_name, body.last_name, body.industry_connected];
 
   db.query(sql, params, (err, result) => {
