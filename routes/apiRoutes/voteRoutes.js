f4e8146efcb73c37ef4e9b71c0b46e7f1d39977b
dhalladay <dave.halladay@outlook.com>
2022-01-13 18:11:30 -0700

    add vote counts route

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -11,9 +11,10 @@ router.post('/vote', ({ body }, res) => {
     return;
   }
 
+  
   const sql = `INSERT INTO votes (voter_id, candidate_id) VALUES (?, ?)`;
   const params = [body.voter_id, body.candidate_id];
-
+  
   db.query(sql, params, (err, result) => {
     if (err) {
       res.status(400).json({ error: err.message });
@@ -23,6 +24,25 @@ router.post('/vote', ({ body }, res) => {
       message: 'success',
       data: body,
       changes: result.affectedRows
+    });
+  });
+});
+
+router.get('/votes', (req, res) => {
+  const sql =`SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) AS count
+              FROM votes
+              LEFT JOIN candidates ON votes.candidate_id = candidates.id
+              LEFT JOIN parties ON candidates.party_id = parties.id
+              GROUP BY candidate_id ORDER BY count DESC`;
+
+  db.query(sql, (err,rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
     });
   });
 });
